2ff9fa63ab52b65600e020f942d722331ab78e0c
Raul Gonzalez <xraaulx@gmail.com>
2022-11-06 23:27:41 -0600

    CRUD de categorias

---
+++ API-DB/Controllers/CategoryController.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API_DB.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        //Conexion
+        private readonly ProductosDBContext _context;
+
+        public CategoryController(ProductosDBContext context)
+        {
+            _context = context;
+        }
+
+        //Leer
+        [HttpGet]
+        public async Task<ActionResult<List<Categorium>>> Get()
+        {
+            //return Ok(await _context.Productos.ToListAsync());
+            return Ok(await _context.Categoria.ToListAsync());
+        }
+
+        //Leer por id
+        [HttpGet("{CatId}")]
+        public async Task<ActionResult<Categorium>> Get(int CatId)
+        {
+            var categoria = await _context.Categoria.FindAsync(CatId);
+            if (categoria == null)
+                return BadRequest("Producto no encontrado");
+            return Ok(categoria);
+        }
+
+
+        //Crear
+        [HttpPost]
+        public async Task<ActionResult<List<Categorium>>> Post([Bind("Ola,aaaa,aaaaaa")] Categorium categoria)
+        {
+            _context.Categoria.Add(categoria);
+            await _context.SaveChangesAsync();
+
+            return Ok(await _context.Categoria.ToListAsync());
+        }
+
+
+        //Actualizar
+        [HttpPut]
+        public async Task<ActionResult<List<Categorium>>> Update([Bind("CatId,CatNom,CatDesc")] Categorium update)
+        {
+
+            var dbCategoria = await _context.Categoria.FindAsync(update.CatId);
+            if (dbCategoria == null)
+                return BadRequest("Producto no encontrado");
+        
+            dbCategoria.CatNom = update.CatNom;
+            dbCategoria.CatDesc = update.CatDesc;
+            
+
+            await _context.SaveChangesAsync();
+
+            return Ok(await _context.Categoria.ToListAsync());
+        }
+
+
+        //Eliminar
+        [HttpDelete("{CatId}")]
+        public async Task<ActionResult<List<Categorium>>> Delete(int CatId)
+        {
+
+            var dbCategoria = await _context.Categoria.FindAsync(CatId);
+            if (dbCategoria == null)
+                return BadRequest("Producto no encontrado");
+
+            _context.Categoria.Remove(dbCategoria);
+            await _context.SaveChangesAsync();
+
+            return Ok(await _context.Categoria.ToListAsync());
+        }
+    }
+}
